24c6c5f60880a4abd89edff5ba0534fac7cbe900
scott <scott.lan@taisys.com>
2018-03-13 18:23:45 +0800

    fix eslint issue

--- src/components/pages/ChatPage/index.js
+++ src/components/pages/ChatPage/index.js
@@ -6,64 +6,78 @@ import { apiUrl } from 'config'
 import socketIOClient from 'socket.io-client'
 
 class ChatPage extends Component {
-    constructor(props) {
-        super(props)
-        console.log(props.location.state)
-        console.log(socketIOClient)
+  constructor(props) {
+    super(props)
+    console.log(props.location.state)
+    console.log(socketIOClient)
 
-        this.state = {
-            // username: self.props.history.location.state.username,
-            // chats: [],
-            // date: new Date()
-            currentMsg: '',
-            // messages: [{ username: 'scott', message: 'hellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohellohello' }],
-            messages: [],
-        }
-        this.onSendMsg = this.onSendMsg.bind(this);
-        this.onInputChange = this.onInputChange.bind(this);
-        // this.socketIoFactory = this.socketIoFactory.bind(this);
+    this.state = {
+      // username: self.props.history.location.state.username,
+      // chats: [],
+      // date: new Date()
+      currentMsg: '',
+      messages: [],
     }
-    // socketIoFactory(){
-    // }
-    componentDidMount(){
-        var self = this;
-        var io = self.io = socketIOClient(apiUrl)
-        // var io = self.io = socketIOClient('http://10.1.101.79:3000/')
-        // debugger;
-        io.on('connect', function() {
-            io.emit('add user', self.props.location.state.username, self.props.location.state.chatId)
-        })
-        io.on('new room chat', function(data) {
-            console.log([data]);
-            var nextMsg = Object.assign([] ,self.state.messages);
-            nextMsg.push(data);
-            self.setState({
-                messages: nextMsg
-            })
-        })
-        io.on('disconnect', function() {
-            console.log('io disconnect');
-        })
-    }
-    componentWillUnmount(){
-        this.io.disconnect()
-    }
-    onSendMsg(){
-        this.io.emit('room chat', this.state.currentMsg);
-    }
-    onInputChange(msg){
-        this.setState({
-            currentMsg: msg
-        })
-    }
-    render(){
-        var self = this
-        return (
-            <ChatTemplate header={<Header title="Chat" icon="left-arrow" iconLink={history.back} />}>
-                <Chat {...self.props} onSendMsg={self.onSendMsg} onInputChange={self.onInputChange} messages={self.state.messages}></Chat>
-            </ChatTemplate>
-        )
-    }
+    this.onSendMsg = this.onSendMsg.bind(this)
+    this.onInputChange = this.onInputChange.bind(this)
+    // this.socketIoFactory = this.socketIoFactory.bind(this);
+  }
+  // socketIoFactory(){
+  // }
+  componentDidMount() {
+    const self = this
+    const io = socketIOClient(apiUrl)
+    self.io = io
+    // var io = self.io = socketIOClient('http://10.1.101.79:3000/')
+    io.on('connect', () => {
+      io.emit(
+        'add user',
+        self.props.location.state.username,
+        self.props.location.state.chatId
+      )
+    })
+    io.on('new room chat', (data) => {
+      console.log([data])
+      const nextMsg = Object.assign([], self.state.messages)
+      nextMsg.push(data)
+      self.setState({
+        messages: nextMsg,
+      })
+    })
+    io.on('disconnect', () => {
+      console.log('io disconnect')
+    })
+  }
+  componentWillUnmount() {
+    this.io.disconnect()
+  }
+  onSendMsg() {
+    this.io.emit('room chat', this.state.currentMsg)
+  }
+  onInputChange(msg) {
+    this.setState({
+      currentMsg: msg,
+    })
+  }
+  render() {
+    const self = this
+    return (
+      <ChatTemplate
+        header={<Header
+          title="Chat"
+          icon="left-arrow"
+          iconLink={history.back}
+        />}
+      >
+        <Chat
+          {...self.props}
+          onSendMsg={self.onSendMsg}
+          onInputChange={self.onInputChange}
+          messages={self.state.messages}
+        />
+      </ChatTemplate>
+    )
+  }
 }
 
 export default ChatPage
